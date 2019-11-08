e8cd003f6522804f9f55689beecc3ff7c624d078
Nqsir <steyaert.victor@gmail.com>
2019-11-08 12:56:08 +0100

    [Fix][Progress] [Fix] < Solved quartiles problem > [Progress] < Finished quartiles and the function, also added docstrings >

--- describe.py
+++ describe.py
@@ -5,9 +5,90 @@ import pandas as pd
 import math
 
 
-def describe(_file):
-    df = pd.read_csv(_file)
-    print(f'{df.describe()}\n\n')
+def quartile_1(col, col_length):
+    """
+    First quartile (25%)
+    :param col: column (DF format)
+    :param col_length: column's length
+    :return: First quartile
+    """
+    Q1 = (col_length - 1) / 4
+    calc = ((Q1 - math.trunc(Q1)) * 100) / 25
+    if Q1.is_integer():
+        return col[Q1]
+    else:
+        return ((col[math.trunc(Q1)] * (4 - calc)) + col[math.ceil(Q1)] * calc) / 4
+
+
+def quartile_2(col, col_length):
+    """
+    Second quartile or median (50%)
+    :param col: column (DF format)
+    :param col_length: column's length
+    :return: Second quartile
+    """
+    Q2 = ((col_length - 1) / 4) * 2
+    calc = ((Q2 - math.trunc(Q2)) * 100) / 25
+    if Q2.is_integer():
+        return col[Q2]
+    else:
+        return ((col[math.trunc(Q2)] * (4 - calc)) + col[math.ceil(Q2)] * calc) / 4
+
+
+def quartile_3(col, col_length):
+    """
+    Third quartile (75%)
+    :param col: column (DF format)
+    :param col_length: column's length
+    :return: Third quartile
+    """
+    Q3 = ((col_length - 1) / 4) * 3
+    calc = ((Q3 - math.trunc(Q3)) * 100) / 25
+    if Q3.is_integer():
+        return col[Q3]
+    else:
+        return ((col[math.trunc(Q3)] * (4 - calc)) + col[math.ceil(Q3)] * calc) / 4
+
+
+def std(col, col_length, mean):
+    """
+    Make the Standard deviation of the observations
+    :param col: column (DF format)
+    :param col_length: column's length
+    :param mean: mean of the col values
+    :return: the Standard deviation of the observations
+    """
+    return (sum([((mean - val) ** 2) for val in col]) / (col_length - 1)) ** (1 / 2)
+
+
+def mean(col, col_length):
+    """
+    Make the mean of the col values
+    :param col: column (DF format)
+    :param col_length: column's length
+    :return: the mean
+    """
+    return col.sum() / col_length
+
+
+def count(col, col_length):
+    """
+    Count the number of non-NaN values
+    :param col: column (DF format)
+    :param col_length: column's length
+    :return: number of non-NaN values
+    """
+    return col_length - col.isna().sum()
+
+
+def describe(df):
+    """
+    Describes a DataFrame, reproduces the behavior of the pandas DataFrame method describe() i.e. Count, Mean, Std, Min,
+    25%, 50% and 75% quartiles, and Max
+    Doesn't take into account NaN values
+    :param df: A DataFrame
+    :return: a new DataFrame with metric for each numerical feature
+    """
     res_count = {}
     res_mean = {}
     res_std = {}
@@ -20,26 +101,13 @@ def describe(_file):
         if df[col_name].dtypes == 'int64' or df[col_name].dtypes == 'float64':
             col = df[col_name].dropna().sort_values().reset_index(drop=True)
             col_length = len(col)
-            print(f'LE FUCKING FUCK = \n{col.describe(percentiles=[.25, ])}')
-            Q_1 = ((col_length + 1) / 4)
-            Q_2 = ((col_length + 1) / 4) * 2 if (((col_length + 1) / 4) * 2).is_integer() \
-                else math.trunc(((col_length - 1) / 4) * 2)
-            Q_3 = ((col_length - 1) / 4) * 3 if (((col_length - 1) / 4) * 3).is_integer() \
-                else math.trunc(((col_length - 1) / 4) * 3)
-            print(f'raw {((col_length - 1) / 4)}, round down {math.trunc(((col_length - 1) / 4))}, round up {math.ceil(((col_length - 1) / 4))}')
-            res_count[col_name] = col_length - col.isna().sum()
-            res_mean[col_name] = col.sum() / col_length
-            res_std[col_name] = (sum([((res_mean[col_name] - val) ** 2) for val in col]) / (col_length - 1)) ** (1 / 2)
+            res_count[col_name] = count(col, col_length)
+            res_mean[col_name] = mean(col, col_length)
+            res_std[col_name] = std(col, col_length, res_mean[col_name])
             res_min[col_name] = col.loc[0]
-            if Q_1.is_integer():
-                res_25[col_name] = col.loc[Q_1]
-            else:
-                print(f'col.loc[loc] = {col.loc[math.trunc(((col_length - 1) / 4))]}\n'
-                      f'col.loc[loc] = {col.loc[math.ceil(((col_length - 1) / 4))]}\n'
-                      f'/2 = {(col.loc[math.trunc(((col_length - 1) / 4))]+col.loc[math.ceil(((col_length - 1) / 4))])/2}\n')
-                res_25[col_name] = (col.loc[math.trunc(((col_length + 1) / 4))] + col.loc[math.ceil(((col_length + 1) / 4))]) / 2
-            res_50[col_name] = col.loc[Q_2]
-            res_75[col_name] = col.loc[Q_3]
+            res_25[col_name] = quartile_1(col, col_length)
+            res_50[col_name] = quartile_2(col, col_length)
+            res_75[col_name] = quartile_3(col, col_length)
             res_max[col_name] = col.loc[col_length - 1]
     return pd.DataFrame([res_count, res_mean, res_std, res_min, res_25, res_50, res_75, res_max],
                         index=['Count', 'Mean', 'Std', 'Min', '25%', '50%', '75%', 'Max'])
@@ -53,6 +121,7 @@ def parsing():
 
     parser = argparse.ArgumentParser(prog='py describe.py')
     parser.add_argument('csv_file', help='A csv file containing data')
+    parser.add_argument('-c', '--compare', action='store_true', help='Comparative mode', default=False)
     _args = parser.parse_args()
 
     return _args
@@ -64,6 +133,10 @@ if __name__ == '__main__':
 
     file = os.path.join(os.getcwd(), args.csv_file)
     if os.path.exists(file)and os.path.isfile(file) and file.endswith('.csv'):
-        print(describe(file))
+        df = pd.read_csv(file)
+        df_described = describe(df)
+        if args.compare:
+            print(f'\x1b[1;30;43mPandas describe():\x1b[0m \n\n{df.describe()}\n\n')
+        print(f'\x1b[1;30;42mDSLR describe.py:\x1b[0m \n\n{df_described}\n\n')
     else:
         sys.exit(print(f'\x1b[1;37;41mThe selected file must be a csv file \x1b[0m\n'))
