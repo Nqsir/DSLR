e7ac60736612026e80d6c7a40bf12c4e82c8b3b8
Nqsir <steyaert.victor@gmail.com>
2019-11-12 20:06:25 +0100

    [Test] [Test] < Function ok, but with df.corr(), need to recalculate covariances to then have the correlation coefficient >

--- scatter_plot.py
+++ scatter_plot.py
@@ -3,6 +3,7 @@ import os
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+from describe import describe
 
 
 def scatter_plot(df):
@@ -12,18 +13,65 @@ def scatter_plot(df):
     """
 
     try:
-        df = df.drop(columns=['Hogwarts House', 'First Name', 'Last Name', 'Birthday', 'Best Hand', 'Index'])
+        df = df.drop(columns=['Hogwarts House', 'First Name', 'Last Name', 'Birthday', 'Best Hand', 'Index'])\
+            .dropna().reset_index(drop=True)
     except KeyError:
         pass
 
-    plt.matshow(abs(df.corr()))
-    plt.xticks(range(len(df.columns)), df.columns, rotation=45)
-    plt.yticks(range(len(df.columns)), df.columns)
-    plt.gca().set_xticks([x - 0.5 for x in plt.gca().get_xticks()][1:], minor='true')
-    plt.gca().set_yticks([y - 0.5 for y in plt.gca().get_yticks()][1:], minor='true')
-    plt.grid(which='minor')
-    plt.colorbar()
-    plt.gcf().subplots_adjust(top=3)
+    columns = list(df)
+    print(columns)
+    col_length = len(df)
+    df_described = describe(df)
+    # -------------- TEST
+    print(df_described.loc['Mean', 'Astronomy'])
+    print(df.cov())
+    for col_name in columns:
+        if df[col_name].dtypes == 'int64' or df[col_name].dtypes == 'float64':
+            for col in columns:
+                print(f'{col_name} -------- {col}')
+                if df[col].dtypes == 'int64' or df[col].dtypes == 'float64':
+                    # print(df_described.loc['Std', col_name] * df_described.loc['Std', col])
+                    i = 0
+                    j = 0
+                    sum_ = 0
+                    while i < df_described.loc['Count', col_name] and j < df_described.loc['Count', col]:
+                        sum_ += (df.loc[i, col_name] - (1 - df_described.loc['Mean', col_name])) *\
+                                (df.loc[j, col] - (1 - df_described.loc['Mean', col]))
+                        i += 1
+                        j += 1
+                    print(f'sum_ = {sum_}')
+                    # one = sum([abs(val - df_described.loc['Mean', col_name]) for val in df[col_name]])
+                    # two = sum([abs(val - df_described.loc['Mean', col]) for val in df[col]])
+                    # print(one)
+                    # print(two)
+                    # print((one * two))
+    #                 # Sum of Squares due to Regression (SSR): Tot((each predicted value - price_mean)²)
+    #                 # SSR = sum([(prediction[i] - p_mean) ** 2 for i in range(nbr_data)])
+    #                 covariance =
+    #                 # Error Sum of Squares (SSE): Tot((each price - each predicted value)²)
+    #                 # SSE = sum([(price[i] - prediction[i]) ** 2 for i in range(nbr_data)])
+    #
+    #                 # R2 = SSR / (SSR + SSE)
+    #                 print(R2)
+
+    # TEST END--------------
+
+    print(df.corr())
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=[5.6, 5])
+    cax = ax.matshow(abs(df.corr()), cmap='Reds')
+    ax.set_title('Correlation coefficients')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.set_xticks(range(len(df.columns.values)))
+    ax.set_yticks(range(len(df.columns.values)))
+    bottom, top = ax.get_ylim()
+    ax.set_ylim(bottom + 0.5, top - 0.5)
+    ax.set_xticklabels(df.columns.values, rotation='vertical')
+    ax.set_yticklabels(df.columns.values)
+    for item in ([ax.xaxis.label, ax.yaxis.label] +
+                 ax.get_xticklabels() + ax.get_yticklabels()):
+        item.set_fontsize(8)
+    fig.colorbar(cax)
+    plt.tight_layout()
 
 
 def parsing():
