00adf46905e7378d20cc602a8a4b1b59db9b0ae4
Nqsir <steyaert.victor@gmail.com>
2019-11-12 12:53:55 +0100

    [Fix] [Fix] < Fixed histogram.py, reinterpreted the subject to make all standard derivations on the same scale >

--- histogram.py
+++ histogram.py
@@ -16,11 +16,17 @@ def std_bar_chart_plot(df):
     except KeyError:
         pass
 
-    if 'Std' in df.index:
-        df = df.sort_values(by='Std', axis=1, ascending=True)
+    if 'Std' in df.index and 'Max' in df.index and 'Min' in df.index:
+        standards = df.loc['Std', :]
+        range_ = df.loc['Max', :] - df.loc['Min', :]
+        perc = (standards / range_) * 100
+        perc = perc.sort_values()
         plt.style.use('ggplot')
+        plt.figure(figsize=[7, 6])
+        plt.gca().set_ylim([0, 35])
         plt.title('Standard Deviation plot (ascending sorted)\nFrom the most to the least homogeneous')
-        plt.bar(list(df), df.loc['Std', :].values)
+        plt.ylabel('Percentage (Standard deviation / (max - min))')
+        plt.bar(perc.index, perc, width=0.5)
         plt.xticks(rotation='vertical')
         plt.tight_layout()
 
