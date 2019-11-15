18feb6520001b6e8ba5d802e7cc08d948894235d
Nqsir <steyaert.victor@gmail.com>
2019-11-15 17:50:00 +0100

    [Progress][Test] [Progress] < Made accuracy_score > [Test] < Trying to make multi linear regression gradient descent >

--- train.py
+++ train.py
@@ -7,6 +7,66 @@ import os
 import seaborn as sns
 import sys
 
+LEARNING_RATE_THETA0 = 0.01
+LEARNING_RATE_THETA1 = 0.01
+LEARNING_RATE_THETA2 = 0.01
+LEARNING_RATE_THETA3 = 0.01
+LEARNING_RATE_THETA4 = 0.01
+CONVERGENCE = 0.001
+
+
+def accuracy_score(list_1, list_2):
+    c = 0
+    length = len(list_1)
+    for i in range(length):
+        if list_1[i] != list_2[i]:
+            c += 1
+    return 1 - (c / length)
+
+
+def gradient_descent(df_x, df_y, list_x):
+    theta0, theta1, theta2, theta3, theta4 = 0, 0, 0, 0, 0
+    prev_theta0, prev_theta1, prev_theta2, prev_theta3, prev_theta4 = 1, 1, 1, 1, 1
+    i = 0
+    nbr_data = len(df_x)
+
+    print(df_x)
+    print(df_y)
+    print(f'number_data = {nbr_data}')
+
+    # eval_it, eval_theta0, eval_theta1 = [], [], []
+    while i < 10:
+        print(i)
+        prev_theta0 = theta0
+        prev_theta1 = theta1
+        prev_theta2 = theta2
+        prev_theta3 = theta3
+        prev_theta4 = theta4
+        tmp_theta0 = LEARNING_RATE_THETA0 * (1 / nbr_data) \
+                     * sum([(theta0 + theta1 * df_x.loc[i, list_x[1]] + theta2 * df_x.loc[i, list_x[2]]
+                             + theta3 * df_x.loc[i, list_x[3]] + theta4 * df_x.loc[i, list_x[4]])
+                            - df_y.loc[i] for i in range(nbr_data)])
+        tmp_theta1 = LEARNING_RATE_THETA1 * (1 / nbr_data)\
+                     * sum([((theta0 + theta1 * df_x.loc[i, list_x[1]]) - df_y.loc[i]) * df_x.loc[i, list_x[1]] for i in range(nbr_data)])
+        tmp_theta2 = LEARNING_RATE_THETA2 * (1 / nbr_data) \
+                     * sum([((theta0 + theta2 * df_x.loc[i, list_x[2]]) - df_y.loc[i]) * df_x.loc[i, list_x[2]] for i in range(nbr_data)])
+        tmp_theta3 = LEARNING_RATE_THETA3 * (1 / nbr_data) \
+                     * sum([((theta0 + theta3 * df_x.loc[i, list_x[3]]) - df_y.loc[i]) * df_x.loc[i, list_x[3]] for i in range(nbr_data)])
+        tmp_theta4 = LEARNING_RATE_THETA4 * (1 / nbr_data) \
+                     * sum([((theta0 + theta4 * df_x.loc[i, list_x[4]]) - df_y.loc[i]) * df_x.loc[i, list_x[4]] for i in range(nbr_data)])
+        theta0 = theta0 - tmp_theta0
+        theta1 = theta1 - tmp_theta1
+        theta2 = theta2 - tmp_theta2
+        theta3 = theta3 - tmp_theta3
+        theta4 = theta4 - tmp_theta4
+        i += 1
+
+    print(f'Nbr iteration = i')
+
+    print(f'theta0 = {theta0} theta1 = {theta1} theta2 = {theta2} theta3 = {theta3} theta4 = {theta4}')
+
+    return theta0, theta1, theta2, theta3, prev_theta4
+
 
 def train(df):
     try:
@@ -20,14 +80,18 @@ def train(df):
     print(f'Test ------------------------------ {list_x_test[1:]}')
     x = df[list_x_test[1:]]
     y = pd.DataFrame(df['Hogwarts House'])
-    y['Hogwarts House'] = np.where(y['Hogwarts House'] == house_list[0], 1, y['Hogwarts House'])
-    y['Hogwarts House'] = np.where(y['Hogwarts House'] == house_list[1], 2, y['Hogwarts House'])
-    y['Hogwarts House'] = np.where(y['Hogwarts House'] == house_list[2], 3, y['Hogwarts House'])
-    y['Hogwarts House'] = np.where(y['Hogwarts House'] == house_list[3], 4, y['Hogwarts House'])
+    for i in range(4):
+        y['Hogwarts House'] = np.where(y['Hogwarts House'] == house_list[i], i + 1, y['Hogwarts House'])
 
     from sklearn.model_selection import train_test_split
     x_train, x_test, y_train, y_test = train_test_split(x, y['Hogwarts House'].astype('int'),
                                                         test_size=0.2, random_state=0)
+
+    # print(df.loc[df['Hogwarts House'] == 'Ravenclaw', list_x_test[1:]].reset_index(drop=True))
+    # print(y.loc[y['Hogwarts House'] == 1].reset_index(drop=True))
+
+    gradient_descent(df.loc[df['Hogwarts House'] == 'Ravenclaw', list_x_test[1:]].reset_index(drop=True),
+                     y.loc[y['Hogwarts House'] == 1].reset_index(drop=True), list_x_test)
 
     from sklearn.linear_model import LogisticRegression
     logreg = LogisticRegression(solver='lbfgs')
@@ -66,8 +130,8 @@ def train(df):
         y_pred.append(key_y)
         # for key in range(1, 4, 1):
     y_pred = np.array(y_pred)
-    print(y_pred)
-    print(logreg.predict(x_test))
+    print(f'Prediction calculee a la main = \n{y_pred}\n')
+    print(f'Prediction sklearn = \n{logreg.predict(x_test)}\n')
     # print(f'list(logreg.coef_[0]) = {list(logreg.coef_[0])}')
 
     from sklearn import metrics
@@ -89,16 +153,14 @@ def train(df):
     plt.xlabel('Predicted label')
     plt.show()
 
-    print(np.where(y_test != 1, 0, y_test))
-    print(np.where(y_pred != 1, 0, y_pred))
     print(f'Accuracy {house_list[0]}:'
-          f'{metrics.accuracy_score(np.where(y_test != 1, 0, y_test), np.where(y_pred != 1, 0, y_pred)) * 100:.2f}%')
+          f'{accuracy_score(np.where(y_test != 1, 0, y_test), np.where(y_pred != 1, 0, y_pred)) * 100:.2f}%')
     print(f'Accuracy {house_list[1]}:'
-          f'{metrics.accuracy_score(np.where(y_test != 2, 0, y_test), np.where(y_pred != 2, 0, y_pred)) * 100:.2f}%')
+          f'{accuracy_score(np.where(y_test != 2, 0, y_test), np.where(y_pred != 2, 0, y_pred)) * 100:.2f}%')
     print(f'Accuracy {house_list[2]}:'
-          f'{metrics.accuracy_score(np.where(y_test != 3, 0, y_test), np.where(y_pred != 3, 0, y_pred)) * 100:.2f}%')
+          f'{accuracy_score(np.where(y_test != 3, 0, y_test), np.where(y_pred != 3, 0, y_pred)) * 100:.2f}%')
     print(f'Accuracy {house_list[3]}:'
-          f'{metrics.accuracy_score(np.where(y_test != 4, 0, y_test), np.where(y_pred != 4, 0, y_pred)) * 100:.2f}%')
+          f'{accuracy_score(np.where(y_test != 4, 0, y_test), np.where(y_pred != 4, 0, y_pred)) * 100:.2f}%')
 
 
 def parsing():
