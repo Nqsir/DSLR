fb19d7e978a32185c28b7c55f0d3b3a940d50ff0
Nqsir <steyaert.victor@gmail.com>
2019-11-19 13:18:30 +0100

    [Progress] [Progress] < Ok working perfectly with more than 98% accuracy, need to clean and organize code now >

--- train.py
+++ train.py
@@ -7,13 +7,6 @@ import os
 import seaborn as sns
 import sys
 
-LEARNING_RATE_THETA0 = 0.01
-LEARNING_RATE_THETA1 = 0.01
-LEARNING_RATE_THETA2 = 0.01
-LEARNING_RATE_THETA3 = 0.01
-LEARNING_RATE_THETA4 = 0.01
-CONVERGENCE = 0.001
-
 
 def accuracy_score(list_1, list_2):
     c = 0
@@ -24,115 +17,114 @@ def accuracy_score(list_1, list_2):
     return 1 - (c / length)
 
 
-def gradient_descent(df_x, df_y, list_x):
-    theta0, theta1, theta2, theta3, theta4 = 0, 0, 0, 0, 0
-    prev_theta0, prev_theta1, prev_theta2, prev_theta3, prev_theta4 = 1, 1, 1, 1, 1
-    i = 0
-    nbr_data = len(df_x)
-
-    print(df_x)
-    print(df_y)
-    print(f'number_data = {nbr_data}')
-
-    # eval_it, eval_theta0, eval_theta1 = [], [], []
-    while i < 10:
-        print(i)
-        prev_theta0 = theta0
-        prev_theta1 = theta1
-        prev_theta2 = theta2
-        prev_theta3 = theta3
-        prev_theta4 = theta4
-        tmp_theta0 = LEARNING_RATE_THETA0 * (1 / nbr_data) \
-                     * sum([(theta0 + theta1 * df_x.loc[i, list_x[1]] + theta2 * df_x.loc[i, list_x[2]]
-                             + theta3 * df_x.loc[i, list_x[3]] + theta4 * df_x.loc[i, list_x[4]])
-                            - df_y.loc[i] for i in range(nbr_data)])
-        tmp_theta1 = LEARNING_RATE_THETA1 * (1 / nbr_data)\
-                     * sum([((theta0 + theta1 * df_x.loc[i, list_x[1]]) - df_y.loc[i]) * df_x.loc[i, list_x[1]] for i in range(nbr_data)])
-        tmp_theta2 = LEARNING_RATE_THETA2 * (1 / nbr_data) \
-                     * sum([((theta0 + theta2 * df_x.loc[i, list_x[2]]) - df_y.loc[i]) * df_x.loc[i, list_x[2]] for i in range(nbr_data)])
-        tmp_theta3 = LEARNING_RATE_THETA3 * (1 / nbr_data) \
-                     * sum([((theta0 + theta3 * df_x.loc[i, list_x[3]]) - df_y.loc[i]) * df_x.loc[i, list_x[3]] for i in range(nbr_data)])
-        tmp_theta4 = LEARNING_RATE_THETA4 * (1 / nbr_data) \
-                     * sum([((theta0 + theta4 * df_x.loc[i, list_x[4]]) - df_y.loc[i]) * df_x.loc[i, list_x[4]] for i in range(nbr_data)])
-        theta0 = theta0 - tmp_theta0
-        theta1 = theta1 - tmp_theta1
-        theta2 = theta2 - tmp_theta2
-        theta3 = theta3 - tmp_theta3
-        theta4 = theta4 - tmp_theta4
-        i += 1
-
-    print(f'Nbr iteration = i')
-
-    print(f'theta0 = {theta0} theta1 = {theta1} theta2 = {theta2} theta3 = {theta3} theta4 = {theta4}')
-
-    return theta0, theta1, theta2, theta3, prev_theta4
-
-
 def train(df):
-    try:
-        df = df.drop(columns=['Index']).dropna().reset_index(drop=True)
-    except KeyError:
-        pass
-
     house_list = ['Ravenclaw', 'Slytherin', 'Gryffindor', 'Hufflepuff']
     list_x_test = ['Hogwarts House', 'Astronomy', 'Herbology', 'Defense Against the Dark Arts', 'Ancient Runes']
 
     print(f'Test ------------------------------ {list_x_test[1:]}')
+    df = df[list_x_test[:]]
+    df = df.dropna().reset_index(drop=True)
+    print(len(df))
     x = df[list_x_test[1:]]
+    x = x.to_numpy()
     y = pd.DataFrame(df['Hogwarts House'])
     for i in range(4):
         y['Hogwarts House'] = np.where(y['Hogwarts House'] == house_list[i], i + 1, y['Hogwarts House'])
 
-    from sklearn.model_selection import train_test_split
-    x_train, x_test, y_train, y_test = train_test_split(x, y['Hogwarts House'].astype('int'),
-                                                        test_size=0.2, random_state=0)
+    y = y.astype('int')
+    spl = 0.8
+    num = len(x)
+    sample = int(spl * num)
+    x_train = x[:sample]
+    x_test = x[sample:]
+    y_train = y[:sample]
+    y_test = y[sample:]
 
-    # print(df.loc[df['Hogwarts House'] == 'Ravenclaw', list_x_test[1:]].reset_index(drop=True))
-    # print(y.loc[y['Hogwarts House'] == 1].reset_index(drop=True))
+    num_examples = x_train.shape[0]
+    num_features = x_train.shape[1]
+    num_labels = 4
+    y_train = np.resize(y_train, num_examples)
+    y_test = np.resize(y_test, y_test.shape[0])
 
-    gradient_descent(df.loc[df['Hogwarts House'] == 'Ravenclaw', list_x_test[1:]].reset_index(drop=True),
-                     y.loc[y['Hogwarts House'] == 1].reset_index(drop=True), list_x_test)
+    X = np.ones(shape=(x_train.shape[0], x_train.shape[1] + 1))
+    X[:, 1:] = x_train
 
-    from sklearn.linear_model import LogisticRegression
-    logreg = LogisticRegression(solver='lbfgs')
-    logreg.fit(x_train, y_train)
-    print(f'θ0 = {logreg.intercept_[0]} {[f"θ{e + 1} = {coeff}" for e, coeff in enumerate(list(logreg.coef_[0]))]}\n\n')
-    print(f'θ0 = {logreg.intercept_[1]} {[f"θ{e + 1} = {coeff}" for e, coeff in enumerate(list(logreg.coef_[1]))]}\n\n')
-    print(f'θ0 = {logreg.intercept_[2]} {[f"θ{e + 1} = {coeff}" for e, coeff in enumerate(list(logreg.coef_[2]))]}\n\n')
-    print(f'θ0 = {logreg.intercept_[3]} {[f"θ{e + 1} = {coeff}" for e, coeff in enumerate(list(logreg.coef_[3]))]}\n\n')
-    y_pred = []
-    x_test = x_test.reset_index(drop=True)
-    for i in range(len(x_test)):
-        list_y = []
-        list_y.append(1 / (1 + math.exp(-(logreg.intercept_[0] + (x_test.loc[i, 'Astronomy'] * logreg.coef_[0][0])
-                      + (x_test.loc[i, 'Herbology'] * logreg.coef_[0][1])
-                      + (x_test.loc[i, 'Defense Against the Dark Arts'] * logreg.coef_[0][2])
-                      + (x_test.loc[i, 'Ancient Runes'] * logreg.coef_[0][3])))))
-        list_y.append(1 / (1 + math.exp(-(logreg.intercept_[1] + (x_test.loc[i, 'Astronomy'] * logreg.coef_[1][0])
-                      + (x_test.loc[i, 'Herbology'] * logreg.coef_[1][1])
-                      + (x_test.loc[i, 'Defense Against the Dark Arts'] * logreg.coef_[1][2])
-                      + (x_test.loc[i, 'Ancient Runes'] * logreg.coef_[1][3])))))
-        list_y.append(1 / (1 + math.exp(-(logreg.intercept_[2] + (x_test.loc[i, 'Astronomy'] * logreg.coef_[2][0])
-                      + (x_test.loc[i, 'Herbology'] * logreg.coef_[2][1])
-                      + (x_test.loc[i, 'Defense Against the Dark Arts'] * logreg.coef_[2][2])
-                      + (x_test.loc[i, 'Ancient Runes'] * logreg.coef_[2][3])))))
-        list_y.append(1 / (1 + math.exp(-(logreg.intercept_[3] + (x_test.loc[i, 'Astronomy'] * logreg.coef_[3][0])
-                      + (x_test.loc[i, 'Herbology'] * logreg.coef_[3][1])
-                      + (x_test.loc[i, 'Defense Against the Dark Arts'] * logreg.coef_[3][2])
-                      + (x_test.loc[i, 'Ancient Runes'] * logreg.coef_[3][3])))))
-        val_y = 0
-        key_y = 0
-        for e, v in enumerate(list_y):
-            if v > val_y:
-                val_y = v
-                key_y = e + 1
+    def sigmoid(z):
+        return 1 / (1 + math.e ** (-z))
 
-        y_pred.append(key_y)
-        # for key in range(1, 4, 1):
-    y_pred = np.array(y_pred)
+    def cost(theta, X, y):
+        predictions = sigmoid(X @ theta)
+        predictions[predictions == 1] = 0.999999
+        predictions[predictions == 0] = 0.000001
+        error = -y * np.log(predictions) - (1 - y) * np.log(1 - predictions)
+        return sum(error) / len(y)
+
+    def cost_gradient(theta, X, y):
+        predictions = sigmoid(X @ theta)
+        return X.transpose() @ (predictions - y) / len(y)
+
+    def gradient_descent(X, y, alpha):
+        num_features = X.shape[1]
+        theta = np.zeros(num_features)
+        i = 0
+        while (cost(theta, X, y) > 0.15 or i < 35000) and i < 50000:
+            theta = theta - (alpha * cost_gradient(theta, X, y))  # compute and record the cost
+            i += 1
+
+        print(i)
+        return theta
+
+    classifiers = np.zeros(shape=(num_labels, num_features + 1))
+    num_iters = 20000
+    for c in range(1, num_labels + 1):
+        label = (y_train == c).astype(int)
+        classifiers[(c - 1), :] = gradient_descent(X, label, 0.000035)
+        print(f'Thetas = {classifiers[(c - 1), :]} for {house_list[c - 1]} with learning_rate = {0.0000008}')
+
+    X_test = np.ones(shape=(x_test.shape[0], x_test.shape[1] + 1))
+    X_test[:, 1:] = x_test
+    probabilities = sigmoid(X_test @ classifiers.transpose())
+    y_pred = probabilities.argmax(axis=1) + 1
+
+    # from sklearn.linear_model import LogisticRegression
+    # logreg = LogisticRegression(solver='lbfgs')
+    # logreg.fit(x_train, y_train)
+    # for i, _ in enumerate(logreg.intercept_):
+    #     print(f'θ0 = {logreg.intercept_[i]} '
+    #           f'{[f"θ{e + 1} = {coeff}" for e, coeff in enumerate(list(logreg.coef_[i]))]}\n\n')
+    # y_pred = []
+    # x_test = x_test.reset_index(drop=True)
+    # for i in range(len(x_test)):
+    #     list_y = []
+    #     list_y.append(1 / (1 + math.exp(-(logreg.intercept_[0] + (x_test.loc[i, 'Astronomy'] * logreg.coef_[0][0])
+    #                   + (x_test.loc[i, 'Herbology'] * logreg.coef_[0][1])
+    #                   + (x_test.loc[i, 'Defense Against the Dark Arts'] * logreg.coef_[0][2])
+    #                   + (x_test.loc[i, 'Ancient Runes'] * logreg.coef_[0][3])))))
+    #     list_y.append(1 / (1 + math.exp(-(logreg.intercept_[1] + (x_test.loc[i, 'Astronomy'] * logreg.coef_[1][0])
+    #                   + (x_test.loc[i, 'Herbology'] * logreg.coef_[1][1])
+    #                   + (x_test.loc[i, 'Defense Against the Dark Arts'] * logreg.coef_[1][2])
+    #                   + (x_test.loc[i, 'Ancient Runes'] * logreg.coef_[1][3])))))
+    #     list_y.append(1 / (1 + math.exp(-(logreg.intercept_[2] + (x_test.loc[i, 'Astronomy'] * logreg.coef_[2][0])
+    #                   + (x_test.loc[i, 'Herbology'] * logreg.coef_[2][1])
+    #                   + (x_test.loc[i, 'Defense Against the Dark Arts'] * logreg.coef_[2][2])
+    #                   + (x_test.loc[i, 'Ancient Runes'] * logreg.coef_[2][3])))))
+    #     list_y.append(1 / (1 + math.exp(-(logreg.intercept_[3] + (x_test.loc[i, 'Astronomy'] * logreg.coef_[3][0])
+    #                   + (x_test.loc[i, 'Herbology'] * logreg.coef_[3][1])
+    #                   + (x_test.loc[i, 'Defense Against the Dark Arts'] * logreg.coef_[3][2])
+    #                   + (x_test.loc[i, 'Ancient Runes'] * logreg.coef_[3][3])))))
+    #     val_y = 0
+    #     key_y = 0
+    #     for e, v in enumerate(list_y):
+    #         if v > val_y:
+    #             val_y = v
+    #             key_y = e + 1
+    #
+    #     y_pred.append(key_y)
+    #
+    # y_pred = np.array(y_pred)
     print(f'Prediction calculee a la main = \n{y_pred}\n')
-    print(f'Prediction sklearn = \n{logreg.predict(x_test)}\n')
-    # print(f'list(logreg.coef_[0]) = {list(logreg.coef_[0])}')
+    print(f'Reel = \n{y_test}\n')
+    # print(f'Prediction sklearn = \n{logreg.predict(x_test)}\n')
 
     from sklearn import metrics
     cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
